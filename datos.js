8e9d4be618e131935256435a0cfe6398bf2dd3e4
carlitos4560 <carlitosrobertoes@gmail.com>
2018-10-20 16:38:15 -0400

    datos del nasa

---
+++ datos.js
@@ -0,0 +1,114 @@
+
+let  gasas =[{ 
+    nombre : "Curitas",
+    prioridad : 1
+    },
+    {
+        nombre : "vendas",
+        prioridad : 1 
+    },
+    {
+        nombre : "Rollos de gaza",
+        prioridad : 1
+    },
+    {
+        nombre : "Hisopos de algodón",
+        prioridad : 1
+    },
+    {
+        nombre : "Rollos de cinta adhesiva",
+        prioridad : 1
+    }
+];
+
+let alimentos = [{
+    manzana: {
+        nombre: "manzana"
+        }
+    
+    }
+];
+let medicamentos=[{
+            medicamentos
+        },
+    // Agua oxigenada para lavar y desinfectar heridas
+    // Ungüento antibiótico
+    // Toallas de alcohol empaquetadas individualmente
+    // Pastillas con y sin aspirina
+    // Medicamentos recetados y de uso prolongado (manténgalos al corriente)
+    // Medicación para la diarrea
+    // Gotas para los ojos
+
+];
+let medicamentos = [{
+            nombre: "aspirina",
+            prioridad: 1,
+            descripcion: "aspirina para dolores de cabeza"
+        },
+        {
+            nombre: "hibuprofeno",
+            prioridad :1,
+            descripcion : "calmante"
+        },{
+            nombre: "amoxisilina",
+            prioridad :1,
+            descripcion : "antiviotico"
+        },
+        {
+            nombre : "alcohol",
+            prioridad :1,
+            descripcion : "desinfectante"
+        },
+        {
+            nombre : "Agua oxigenada",
+            prioridad :1,
+            descripcion : "para lavar y desinfectar heridas" 
+        }     
+
+];
+
+let metalicos = [{
+        nombre : "tijeras",
+        tamanio : 3.5,
+        prioridad: 1
+    },
+    {
+        nombre : "pinzas",
+        tamanio : "pequenio",
+        prioridad: 1
+    },
+    {
+        nombre : "Navaja de bolsillo",
+        tamanio : "pequenio",
+        prioridad: 1
+    },
+];
+
+let utensilios = [{
+    cortantes : cortantes,
+    termometro : {
+        nombre : "termometro",
+        prioridad : 1,
+        descripcion : "para medir la temperatura"
+        
+    },
+    libro : {
+        nombre : "El buen samaritano",
+        prioridad :1,
+        descripcion : "consultar en caso de accidente"
+
+    }
+    // Libro de primeros auxilios
+    // Termómetro
+    // Barra de jabón
+    // Pañuelos de papel
+    // Protector solar
+    // Vasos de papel
+    // Bolsas de plástico pequeñas
+    // Alfileres de seguridad
+    // Hilo y agujas
+    // Paquetes enfriadores instantáneos para esguinces
+    // Toallas sanitarias
+    // Materiales para entablillar
+
+}];

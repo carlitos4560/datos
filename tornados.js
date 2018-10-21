ab77fed78522451ff23fa48e87743bbf404df56d
carlitos4560 <carlitosrobertoes@gmail.com>
2018-10-20 23:53:41 -0400

    add tornados

---
+++ tornados.js
@@ -0,0 +1,165 @@
+let gasas = [{
+    nombre: "Curitas",
+    descripcion: "para heridas leves"
+},
+{
+    nombre: "vendas",
+    descripcion: "Ayudan a recubrir heridas"
+},
+{
+    nombre: "Rollos de gaza",
+    descripcion: "Tambien conocidos como vendas"
+},
+{
+    nombre: "Hisopos de algodón",
+    descripcion: "Ayuda al limpiado de heridas"
+},
+{
+    nombre: "Rollos de cinta adhesiva",
+    descripcion: "Ayuda a sostener las vendas"
+}
+];
+
+
+let alimentos = [{
+    agua: {
+        nombre: "Agua",
+        descripcion: "3.9 Litros por persona"
+    }
+},
+{
+    galletas: {
+        nombre: "Galletas",
+        descripcion: "galletas integrales o galletas no saladas"
+    }
+},
+{
+    enlatados: {
+        nombre: "Enlatados/Conservas",
+        descripcion: "Enlatados de atún, carne, frutas, otros."
+    }
+},
+{
+    golosinas: {
+        nombre: "Golosinas",
+        descripcion: "Golosinas"
+    }
+},{
+    racion_ceca :{
+        nombre : "conservas",
+        descripcion: "en casos de perdida para super vivencia"
+    }
+}
+];
+let medicamentos = [{
+    nombre: "aspirina",
+    descripcion: "aspirina para dolores de cabeza"
+},
+{
+    nombre: "ibuprofeno",
+    descripcion: "calmante"
+}, {
+    nombre: "amoxicilina",
+    descripcion: "antibiotico"
+},
+{
+    nombre: "alcohol",
+    descripcion: "desinfectante"
+},
+{
+    nombre: "Agua oxigenada",
+    descripcion: "para lavar y desinfectar heridas"
+},
+{
+    nombre: "Ungento ",
+    descripcion: "antibiotico"
+},
+{
+    nombre: "Toallas de Alcohol",
+    descripcion: "para desinfectar heridas"
+},
+{
+    nombre: "Diarreicos",
+    descripcion: "evitan la diarrea"
+},
+];
+
+let metalicos = [{
+    nombre: "tijeras",
+    tamanio: 3.5,
+},
+{
+    nombre: "pinzas",
+    tamanio: "pequenio",
+},
+{
+    nombre: "Navaja de bolsillo",
+    tamanio: "pequenio",
+},
+];
+
+let utensilios = [{
+    linterna: {
+        nombre: "Linterna",
+        descripcion: "linterna con pitas cargadas"
+    },
+    mapa: {
+        nombre: "Mapa",
+        descripcion: "Util para entrar el camino a casa"
+    },
+    silbato: {
+        nombre: "silbato",
+        descripcion: "Para alertar que estamos atrapados,heridos o pedir ayuda"
+    },
+    abrelatas: {
+        nombre: "Abre latas",
+        descripcion: "abrelatas para alimentos"
+    },
+    telefono: {
+        nombre: "telefono",
+        descripcion: "en casos donde se tenga señal y poder realizar una llamada"
+    }
+}];
+
+let documentos = [
+    {
+        pasaporte: {
+            nombre: "Pasaporte",
+            descripcion: "Pasaporte "
+        }
+    },
+    {
+        certificadoDeNacimiento: {
+            nombre: "Certificado de nacimiento",
+            descripcion: ""
+        }
+    },
+    {
+        certificadoDeTítulosAcadémicos: {
+            nombre: "Certificado de títulos académicos",
+            descripcion: ""
+        }
+    },
+    {
+        carnetDeVacunas: {
+            nombre: "Certificado de enfermedades alergias vacunas",
+            descripcion: ""
+        }
+    },
+    {
+        ci: {
+            nombre: "Cédula de identidad",
+            descripcion: "Cédula de identidad o fotocopia"
+        }
+    }
+];
+// datos del botiquin
+let botiquin = {
+    nombre: "tornado",
+    gasas: gasas,
+    medicamentos: medicamentos,
+    metalicos: metalicos,
+    utensilios: utensilios,
+    documentos: documentos
+}
+

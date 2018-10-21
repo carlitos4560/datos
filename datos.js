a36ba0404a1b50837291692d4667b1e3838f1470
carlitos4560 <carlitosrobertoes@gmail.com>
2018-10-20 21:36:38 -0400

    agregando datos de sunami

--- datos.js
+++ datos.js
@@ -1,114 +1,133 @@
-
 let  gasas =[{ 
     nombre : "Curitas",
     prioridad : 1
     },
     {
         nombre : "vendas",
-        prioridad : 1 
+        descripcion : "Ayudan a recubrir heridas"
     },
     {
         nombre : "Rollos de gaza",
-        prioridad : 1
+        descripcion : "Tambien conocidos como vendas"
     },
     {
         nombre : "Hisopos de algodón",
-        prioridad : 1
+        descripcion : "Ayuda al limpiado de heridas"
     },
     {
         nombre : "Rollos de cinta adhesiva",
-        prioridad : 1
+        descripcion : "Ayuda a sostener las vendas"
     }
 ];
 
-let alimentos = [{
-    manzana: {
-        nombre: "manzana"
-        }
-    
-    }
-];
-let medicamentos=[{
-            medicamentos
-        },
-    // Agua oxigenada para lavar y desinfectar heridas
-    // Ungüento antibiótico
-    // Toallas de alcohol empaquetadas individualmente
-    // Pastillas con y sin aspirina
-    // Medicamentos recetados y de uso prolongado (manténgalos al corriente)
-    // Medicación para la diarrea
-    // Gotas para los ojos
-
-];
 let medicamentos = [{
             nombre: "aspirina",
-            prioridad: 1,
             descripcion: "aspirina para dolores de cabeza"
         },
         {
-            nombre: "hibuprofeno",
+            nombre: "ibuprofeno",
             prioridad :1,
             descripcion : "calmante"
         },{
-            nombre: "amoxisilina",
-            prioridad :1,
-            descripcion : "antiviotico"
+            nombre: "amoxicilina",
+            descripcion : "antibiotico"
         },
         {
             nombre : "alcohol",
-            prioridad :1,
             descripcion : "desinfectante"
         },
         {
             nombre : "Agua oxigenada",
-            prioridad :1,
             descripcion : "para lavar y desinfectar heridas" 
-        }     
+        },     
+        {
+            nombre : "Ungento ",
+            descripcion : "antiviotico"
+        },
+        {
+            nombre : "Toallas de Alcohol",
+            descripcion : "para desinfectar heridas"
+        },
+        {
+            nombre : "Diarreicos",
+            descripcion: "evitan la diarrea"
+        },
+        {
+            nombre : "Gotas",
+            descripcion:"Gotas para los ojos"
+        }
 
 ];
 
 let metalicos = [{
         nombre : "tijeras",
         tamanio : 3.5,
-        prioridad: 1
     },
     {
         nombre : "pinzas",
         tamanio : "pequenio",
-        prioridad: 1
     },
     {
         nombre : "Navaja de bolsillo",
         tamanio : "pequenio",
-        prioridad: 1
     },
 ];
 
 let utensilios = [{
-    cortantes : cortantes,
     termometro : {
         nombre : "termometro",
-        prioridad : 1,
-        descripcion : "para medir la temperatura"
-        
+        descripcion : "para medir la temperatura"        
     },
     libro : {
-        nombre : "El buen samaritano",
-        prioridad :1,
-        descripcion : "consultar en caso de accidente"
-
+        nombre : "Primeros Auxilicios el buen doctor",
+        descripcion : "Libro de consulta en caso de accidente"
+    },
+    jabon :{
+        nombre : "Jabon",
+        descripcion : "Ayuda en el lavado de heridas"
+    },
+    pañuelos :{
+        nombre : "Paños de papel",
+        descripcion : "Usados para limpiar heridas"
+    },
+    protector_solar : {
+        nombre : "Protector solar",
+        descripcion : "Crema para protejerce de los rayos utra violeta"
+    },
+    vasos_de_papel : {
+        nombre : "Vasos de papel",
+        descripcion : "usados en momentos de sed "
+    },
+    Bolsas_plastias :{
+        nombre : "bolsas plasticas",
+        descripcion : "para levantar cosas que necesiten llevarse"
+    },
+    alfiler: {
+        nombre : "alfiler",
+        descripcion : "sirve para sujetar las vendas "
+    },
+    hilo_aguja : {
+        nombre : "hilo y aguja",
+        descripcion : "para suturar heridas"
+    },
+    enfriadores : {
+        nombre : "enfriadores",
+        descripcion : "sirve para bajar la temperatura del cuerpo "
+    },
+    toallas_sanitarias :{
+        nombre : "toallas sanitarias",
+        descripcion : "para hacer precion sobre emorageas"
+    },
+    tablillas : {
+        nombre : "tablillas",
+        descripcion :"en casos de entablillar una fracturua"
     }
-    // Libro de primeros auxilios
-    // Termómetro
-    // Barra de jabón
-    // Pañuelos de papel
-    // Protector solar
-    // Vasos de papel
-    // Bolsas de plástico pequeñas
-    // Alfileres de seguridad
-    // Hilo y agujas
-    // Paquetes enfriadores instantáneos para esguinces
-    // Toallas sanitarias
-    // Materiales para entablillar
-
 }];
+// datos del botiquin
+let botiquin = {
+    nombre : "sunamis",
+    gasas : gasas,
+    medicamentos : medicamentos,
+    metalicos : metalicos,
+    utensilios : utensilios
+}
